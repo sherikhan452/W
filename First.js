77ebf90dd194748a2b681a3fc000b885a53d05b5
sk <you@example.com>
2024-08-13 18:18:25 +0500

    Done

---
+++ First.js
@@ -0,0 +1,13 @@
+var a = 123445;
+var b = "Alpha";
+var c = 12;
+var result = a + c;
+var e = [];
+e[0] = 12;
+e[1] = 34;
+e[2] = 54;
+// console.log("The num is "+a+" B is: "+b+"The addition is: "+result);
+function abc(a, b) {
+    console.log(a + b);
+}
+abc(12, 12);
---
+++ First.ts
@@ -0,0 +1,50 @@
+var a:number=12_34_45;
+var b:string="Alpha";
+
+var c:number=12;
+
+var result=a+c;
+
+
+var e:number[]=[];
+e[0]=12;
+e[1]=34;
+e[2]=54;
+
+
+
+
+// console.log("The num is "+a+" B is: "+b+"The addition is: "+result);
+
+
+function abc(a:number,b:number):any{
+
+  console.log(a+b);
+
+}
+
+
+function abcd(a:number,b:number):any{
+
+    console.log(a+b);
+  
+  }
+
+  
+  function abcde(a:number,b:number):any{
+
+    console.log(a+b);
+  
+  }
+
+  
+
+  function abcf(a:number,b:number):any{
+
+    console.log(a+b);
+  
+  }
+  
+
+abc(12,12);
+
